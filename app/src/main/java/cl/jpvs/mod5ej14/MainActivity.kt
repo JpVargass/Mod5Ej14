7d89729ad66b3c630906e7fa5a827d0ee2e8e0b0
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-18 00:07:31 -0400

    App terminada (orden de codigo)

--- app/src/main/java/cl/jpvs/mod5ej14/MainActivity.kt
+++ app/src/main/java/cl/jpvs/mod5ej14/MainActivity.kt
@@ -12,45 +12,47 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnOk.setOnClickListener{
-            when (binding.radioGroup.checkedRadioButtonId){
-             R.id.rbtnVerSaldo ->
-                 binding.textMonto.setText(saldo.toString())
-             R.id.rbtnIngresar -> ingresarSaldo()
-             R.id.rbtnRetirar -> retirarSaldo()
+        binding.btnOk.setOnClickListener {
+            when (binding.radioGroup.checkedRadioButtonId) {
+                R.id.rbtnVerSaldo ->
+                    binding.textMonto.setText(saldo.toString())
+
+                R.id.rbtnIngresar -> ingresarSaldo()
+                R.id.rbtnRetirar -> retirarSaldo()
 
 
             }
         }
 
     }
+
     private fun ingresarSaldo() {
         saldo += binding.textMonto.text.toString().toInt()
         crearMensaje("Su saldo ha sido incrementado")
         limpiarMonto()
 
     }
+
     private fun retirarSaldo() {
-       val montoGiro = binding.textMonto.text.toString().toInt()
-        if (montoGiro <= saldo)
-        {
+        val montoGiro = binding.textMonto.text.toString().toInt()
+        if (montoGiro <= saldo) {
             saldo -= montoGiro
             limpiarMonto()
             crearMensaje("El retiro ha sido realizado correctamente")
 
-        } else  {
+        } else {
             crearMensaje("El retiro no ha sido realizado , saldo Insuficiente")
         }
-          }
+    }
 
     private fun limpiarMonto() {
         binding.textMonto.text.clear()
     }
 
 
-    fun crearMensaje(texto: String ){
-        Toast.makeText(applicationContext,texto, Toast.LENGTH_LONG).show()
+    fun crearMensaje(texto: String) {
+        Toast.makeText(applicationContext, texto, Toast.LENGTH_LONG).show()
     }
 }
